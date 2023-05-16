264d3109e8da97ffa6355659ce738d85814531b6
Michael Aroworade <Ayoaro85@gmail.com>
2023-05-16 06:24:00 -0700

    5. Store first object

--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -2,7 +2,9 @@
 from models.base_model import BaseModel
 
 my_model = BaseModel()
-my_model.name = "My_First_Model"
+my_model.firstname = "Michael"
+my_model.lastname = "Aroworade"
+my_model.occupation = "Software Engineer"
 my_model.my_number = 89
 print(my_model.id)
 print(my_model)
